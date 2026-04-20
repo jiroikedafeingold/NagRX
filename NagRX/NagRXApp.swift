2f18fc91556bcdf0b376142b105bbc5fd973b09e
Jiro Feingold <jiro@feingold5.com>
2026-04-20 15:26:59 -0700

    Initial Commit

---
+++ NagRX/NagRXApp.swift
@@ -0,0 +1,17 @@
+//
+//  NagRXApp.swift
+//  NagRX
+//
+//  Created by Jiro on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct NagRXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
